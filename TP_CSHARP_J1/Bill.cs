30745a0c898f4fb89d42fc4d83ea1486a15805d6
Elvariand <95416175+Elvariand@users.noreply.github.com>
2024-09-30 23:40:02 +0200

    add class Bill

--- TP_CSHARP_J1/Bill.cs
+++ TP_CSHARP_J1/Bill.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace TP_CSHARP_J1
 {
-    internal class Bill
+    public class Bill
     {
+        private string billNo {  get; set; }
+        private DateTime date {  get; set; }
+        private int memberId { get; set; }
+        private double amount { get; set; }
+
+        public Bill(int memberId, double amount)
+        {
+            this.date = DateTime.Now;
+            this.billNo = date.ToString("yyyymmdd") + "-" + new Random().Next(1, 10000000) as string;
+            this.memberId = memberId;
+            this.amount = amount;
+        }
+
+    public override string ToString()
+    {
+            return "receipt n° " + this.billNo + " amounting to " + this.amount + "€ adressed to member n° " + this.memberId + ".";
+    }
+
+    // CRUD Bill
     }
 }
